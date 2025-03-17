8a0de4440a6bb508ee1bd3a5ea70f04fd3cf31ae
kyongseo <pokj930@naver.com>
2025-03-17 11:14:33 +0900

    feat: init commit

---
+++ src/test/java/hdssecu/com/logdata/LogDataApplicationTests.java
@@ -0,0 +1,13 @@
+package hdssecu.com.logdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
